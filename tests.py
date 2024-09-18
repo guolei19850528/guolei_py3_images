ebd82e5c257c05fdd088c723a805ea49bbcc2e56
guolei <174000902@qq.com>
2024-09-18 16:41:46 +0800

    update

--- tests.py
+++ tests.py
@@ -4,14 +4,15 @@ import string
 import unittest
 
 from guolei_py3_images.imgkit import str_to_image
+from guolei_py3_images.v1.imgkit import string_to_image
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
 
-        output_path = str_to_image(
+        output_path = string_to_image(
             output_path=os.path.join(os.path.dirname(__file__), "runtime", 'test.png'),
-            content="".join(random.choices(string.ascii_letters,k=100))
+            string="".join(random.choices(string.ascii_letters,k=100))
         )
         print(output_path)
         self.assertTrue(True, "test failed")  # add assertion here
