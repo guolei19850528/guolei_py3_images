3c8c6b7a34dad1e1f7f72fef0a531318a9d7803f
guolei <174000902@qq.com>
2024-09-19 10:47:35 +0800

    update

--- tests.py
+++ tests.py
@@ -9,7 +9,6 @@ from guolei_py3_images.v1.imgkit import from_string
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-
         output_path = from_string(
             output_path=os.path.join(os.path.dirname(__file__), "runtime", 'test.png'),
             string="".join(random.choices(string.ascii_letters,k=100))
